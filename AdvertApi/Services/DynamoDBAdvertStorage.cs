713a9bbb2f01662d10ce15381a943ffb8e6ee87b
Alexandra <50830164+Alex-Mackol@users.noreply.github.com>
2022-10-05 20:31:46 +0300

    init advert microservice

---
+++ AdvertApi/Services/DynamoDBAdvertStorage.cs
@@ -0,0 +1,17 @@
+﻿using AdvertApi.Models;
+
+namespace AdvertApi.Services
+{
+    public class DynamoDBAdvertStorage : IAdvertStorageService
+    {
+        public Task<string> AddAsync(AdvertModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ConfirmAsync(ConfirmAdvertModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
